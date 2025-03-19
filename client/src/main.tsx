6f4e4eaedb5096351fa299c93c9093d336bd3f0d
SahakK <sahak.keshishyan.dev@gmail.com>
2025-03-19 14:35:40 +0400

    set up dependencies and components

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { theme } from "./theme";
+
+import { App } from "@/components/app/App";
+import { queryClient } from "@/react-query/queryClient";
+
+const root = document.getElementById("root");
+
+if (root) {
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ChakraProvider>
+    </React.StrictMode>
+  );
+}
